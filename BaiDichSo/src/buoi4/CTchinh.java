f6a9783ff96b6f6b3ad4cb5a26a79d5a60c31b4a
Linh02 <nhatlinh3108ooo@gmail.com>
2022-04-04 20:07:36 +0700

    first commit

---
+++ BaiDichSo/src/buoi4/CTchinh.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Buoi4;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author USER
+ */
+public class CTchinh {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        ServerSocket socketServer = new ServerSocket(9999);
+        System.out.println("I'm listening on 9999 port...");
+        //
+        int id=0;
+        while(true){
+            Socket soc = socketServer.accept();
+            //id ++;
+            LuongLamViec luongTask = new LuongLamViec(soc,id);
+            luongTask.start();
+        }  
+    }  
+}
